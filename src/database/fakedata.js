f7c48e85be3f283a858e8e62175ae2c457b03ee7
jackson.emiliano <jackson.alves@fatec.sp.gov>
2021-01-31 17:59:47 -0300

    Projeto Happy

---
+++ src/database/fakedata.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: 1,
+        name: "Lar das meninas",
+        description: "Presta auxilio a crianças de 0 a 16",
+        images:[
+            "https://images.unsplash.com/photo-1608540784734-492895ad910c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ]
+
+    },
+    {
+        id: 2,
+        name: "Lar dos meninos",
+        description: "Presta auxilio a crianças de 0 a 16",
+        images: [
+            "https://images.unsplash.com/photo-1607166602071-847b7d110781?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ]
+
+    }
+]
